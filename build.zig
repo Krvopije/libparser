e8c54fb2c40a97851dec2d3ad46e11a268c02132
Ante Mamic <ante.mamic@protonmail.com>
2023-07-25 19:46:54 +0200

    Missed mod declaration in the build.zig file

--- build.zig
+++ build.zig
@@ -15,6 +15,11 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    const mod = b.addModule("libparser", .{
+        .source_file = .{ .path = "src/parser.zig" },
+    });
+    _ = mod;
+
     const lib = b.addStaticLibrary(.{
         .name = "parser",
         // In this case the main source file is merely a path, however, in more
