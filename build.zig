9655ccc039db65585c61308c39884524e745b55e
Ante Mamic <ante.mamic@protonmail.com>
2024-05-21 22:37:25 +0200

    Another change for the 0.13.0 release

--- build.zig
+++ build.zig
@@ -23,7 +23,7 @@ pub fn build(b: *std.Build) void {
         .name = "parser",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/parser.zig" },
+        .root_source_file = b.path("src/parser.zig"),
         .target = target,
         .optimize = optimize,
     });
