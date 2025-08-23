2c7700b3a588a0bfaf763fb4f57eec1fd121640f
Krvopije <34864681+Krvopije@users.noreply.github.com>
2025-08-23 02:58:24 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -21,7 +21,7 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(.{
+    const lib = b.addLibrary(.{
         .name = "parser",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
